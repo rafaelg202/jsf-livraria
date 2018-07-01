ff669459f96991a70c4f6c001e34fd912021dfa4
Rafael <rafaelg202@gmail.com>
2018-07-01 16:53:53 -0300

    Form Livro

--- src/livraria/bean/LivroBean.java
+++ src/livraria/bean/LivroBean.java
@@ -1,17 +1,56 @@
 package livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 
 import livraria.dao.DAO;
+import livraria.modelo.Autor;
 import livraria.modelo.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
 	
+
+	private Integer autorId;
+	
 	public Livro getLivro() {
 		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+	
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	
+	public List<Livro> getLivros() {
+		return new DAO<Livro>(Livro.class).listaTodos();
+	}
+	
+	public List<Autor> getAutores(){
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public List<Autor> getAutoresDoLivro(){
+		return this.livro.getAutores();
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
 	}
 
 	public void gravar() {
@@ -21,5 +60,7 @@ public class LivroBean {
 			throw new RuntimeException("Livro deve ter pelo menos um autor");
 		}
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		
+		this.livro = new Livro();
 	}
 }
